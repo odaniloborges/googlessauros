48bdd4c3c7fc485ac142e2643bcd5544ff2a5869
Danilo Borges <danilo.cb@live.com>
2024-09-18 13:33:25 -0300

    Add files via Upload

---
+++ script.js
@@ -0,0 +1,65 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um Dino</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let periodo = "";
+    let tamanho = "";
+    let alimentacao = "";
+    let peso = "";
+    let habitat = "";
+    let curiosidades = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        periodo = dado.periodo.toLowerCase()
+        tamanho = dado.tamanho.toLowerCase()
+        alimentacao = dado.alimentacao.toLowerCase()
+        peso = dado.peso.toLowerCase()
+        habitat = dado.habitat.toLowerCase()
+        curiosidades = dado.curiosidades.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || periodo.includes(campoPesquisa) || tamanho.includes(campoPesquisa) || alimentacao.includes(campoPesquisa) || peso.includes(campoPesquisa) || habitat.includes(campoPesquisa) || curiosidades.includes(campoPesquisa) || tags.includes(campoPesquisa))   {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <p class="descricao-meta">${dado.periodo}</p>
+                <p class="descricao-meta">${dado.alimentacao}</p>
+                <p class="descricao-meta">${dado.tamanho}</p>
+                <p class="descricao-meta">${dado.peso}</p>
+                <p class="descricao-meta">${dado.habitat}</p>
+                <p class="descricao-meta">${dado.curiosidades}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
